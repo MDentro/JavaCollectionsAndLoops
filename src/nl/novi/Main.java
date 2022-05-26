7ee35cc47c5c9d66e2e72c8319fd0a7f7d03eace
MDentro <michelle.dentro1@gmail.com>
2022-05-26 11:50:28 +0200

    feed: effects

--- src/nl/novi/Main.java
+++ src/nl/novi/Main.java
@@ -1,8 +1,36 @@
 package nl.novi;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] numeric = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        String[] alphabetic = {"een", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "nul"};
+        Boolean play = true;
+        String ongeldig = "Ongeldige invoer";
+
+        Translator translate = new Translator(numeric, alphabetic);
+        Scanner scannerInput = new Scanner(System.in);
+        Scanner scannerNumber = new Scanner(System.in);
+
+        while (play) {
+            System.out.println("Type 'x' om te stoppen \nType 'v' om te vertalen");
+            String input = scannerInput.nextLine();
+
+            if (input.equalsIgnoreCase("x")) {
+                play = false;
+            } else if (input.equalsIgnoreCase("v")) {
+                System.out.println("Type een cijfer in van 0 t/m 9");
+                int number = scannerNumber.nextInt();
+                if (number < 10) {
+                    translate.translate(number);
+                } else {
+                    System.out.println(ongeldig);
+                }
+            } else {
+                System.out.println(ongeldig);
+            }
+        }
     }
 }
